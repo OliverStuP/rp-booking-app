bf7f655ff5cd8a8ce00d806f5dd2a77304c9f533
NoharOSP <opage77@gmail.com>
2026-08-09 15:48:24 +0200

    Change Date to Time

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,18 @@ import React from 'react';
 import { StyleSheet, SafeAreaView, Dimensions, ScrollView } from 'react-native';
 import RPHeader from './components/SPBHeader.tsx';
 import RPFooter from './components/RPFooter.tsx';
-import RPContact from './screens/RPContact.tsx';
-import About from './screens/About.tsx';
-import Date from './screens/Date.tsx';
+import Time from './screens/Time.tsx';
 
 const App = () => {
 
-  const [stage, setStage] = React.useState("date");
+  const [stage, setStage] = React.useState("time");
 
   return (
     <SafeAreaView tabIndex={0} role='main' style={styles.container}>
       <RPHeader />
       <ScrollView tabIndex={0}>
-        {stage == "date" &&
-          <Date />
+        {stage == "time" &&
+          <Time />
         }
       </ScrollView>
       <RPFooter homePress={() => setStage("landing")} contactPress={() => setStage("contact")} aboutPress={() => setStage("about")} />
